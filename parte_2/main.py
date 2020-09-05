754fa30dfa5e2730bc1ce35e5e362c6420c3398f
Rocha Lima <jonasrla@gmail.com>
2020-09-05 18:25:19 -0300

    Adds argument to main

--- parte_2/main.py
+++ parte_2/main.py
@@ -1,8 +1,14 @@
+from argparse import ArgumentParser
+
 from Context import get_context
 from Reader import read_file
 
 if __name__ == '__main__':
-    context = get_context(file_name)
-    raw_data = read_file(file_name)
+    parser = ArgumentParser(description='Process events')
+    parser.add_argument('file_path', type=str)
+    file_path = parser.parse_args().file_path
+
+    context = get_context(file_path)
+    raw_data = read_file(file_path)
     new_data = context.transformation(raw_data)
     context.save(new_data)
