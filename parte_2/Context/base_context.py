b50f96e96d64d9faa5e5e03bd0e9a7b845c9f946
Rocha Lima <jonasrla@gmail.com>
2020-09-06 22:27:17 -0300

    process new clients

--- parte_2/Context/base_context.py
+++ parte_2/Context/base_context.py
@@ -1,7 +1,22 @@
 from abc import ABC, abstractmethod
+from uuid import uuid4
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
 
 class BaseContext(ABC):
+    def __init__(self):
+        self.spark = SparkSession.builder \
+                                 .appName(self.app_name) \
+                                 .master('local') \
+                                 .config('spark.jars',
+                                         '/Users/Jonas/git_storage/desafio_youse/parte_2/libs/sqlite-jdbc-3.32.3.2.jar') \
+                                 .getOrCreate()
+
+        self.create_id = udf(lambda: str(uuid4()))
+
     def transformation(self, data):
         return data
+
     def save(self, data):
         return 'success'
--- parte_2/Context/create_order_context.py
+++ parte_2/Context/create_order_context.py
@@ -1,4 +1,40 @@
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType
+
 from .base_context import BaseContext
 
 class CreateOrderContext(BaseContext):
-    pass
+    def __init__(self):
+        self.input_schema = StructType([
+            StructField('routing_key', StringType(), False),
+            StructField('message_id', StringType(), False),
+            StructField('raw_timestamp', IntegerType(), False),
+            StructField('payload', StructType([
+                StructField('order_uuid', StringType(), False),
+                StructField('insurance_type', StringType(), False),
+                StructField('sales_channel', StringType(), False),
+                StructField('lead_person', StructType([
+                    StructField('name', StringType(), False),
+                    StructField('phone', StringType(), False),
+                    StructField('email', StringType(), False),
+                ]), False)
+            ]), False)
+        ])
+
+        self.app_name = 'Process Create Order'
+        super().__init__()
+
+    def transformation(self, data):
+        df_input = self.spark.createDataFrame(data, self.input_schema)
+        df_input = df_input.withColumn('client_id', self.create_id())
+
+        df_client = df_input.select('client_id', 'payload.lead_person.*')
+        df_client = df_client.withColumnRenamed('client_id', 'id')
+
+        return df_client
+
+    def save(self, data):
+        data.write.jdbc(url='jdbc:sqlite:datalake.db',
+                        table='clients',
+                        mode='append',
+                        properties={"driver":"org.sqlite.JDBC"})
+        return 'Success'
