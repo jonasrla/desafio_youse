5aaecd79182514f971193a2c2596169b5d827729
Rocha Lima <jonasrla@gmail.com>
2020-09-07 08:38:12 -0300

    Clean up base_context

--- parte_2/Context/base_context.py
+++ parte_2/Context/base_context.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from uuid import uuid4
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
@@ -10,7 +9,7 @@ class BaseContext(ABC):
                                  .appName(self.app_name) \
                                  .master('local') \
                                  .config('spark.jars',
-                                         '/Users/Jonas/git_storage/desafio_youse/parte_2/libs/sqlite-jdbc-3.32.3.2.jar') \
+                                         'libs/sqlite-jdbc-3.32.3.2.jar') \
                                  .getOrCreate()
 
 
